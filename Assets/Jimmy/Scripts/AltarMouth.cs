8b297321f7348dc38358adce487c207f7aabbdb5
Jimmy Che <j2che@ucsd.edu>
2026-05-13 00:37:42 -0700

    dontcrash

---
+++ Assets/Jimmy/Scripts/AltarMouth.cs
@@ -0,0 +1,22 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+public class AltarMouth : UdonSharpBehaviour
+{
+    public GameManager gameManager;
+    public AudioSource munchSound;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        FishPrefab fish = other.GetComponent<FishPrefab>();
+        if (fish != null)
+        {
+            gameManager.SacrificeFish(fish.fishValue);
+            if (munchSound != null) munchSound.Play();
+            
+            // Delete the fish object
+            Networking.Destroy(other.gameObject);
+        }
+    }
+}
